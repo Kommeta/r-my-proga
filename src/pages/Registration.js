afba8155d364587d84853c54990c2b9493c81249
Tat_Kama <tattjana_@mail.ru>
2023-01-09 20:32:10 +0300

    fix: правка состояний инпутов для страницы регистрации/входа

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,167 +1,167 @@
-import axios from "axios";
-import React from "react";
-import { useState, useEffect } from "react";
-import { NavLink, useNavigate } from "react-router-dom";
-import ButtonMain from "../components/UI/button/ButtonMain";
-import MyInput from "../components/UI/input/MyInput";
+// import axios from "axios";
+// import React from "react";
+// import { useState, useEffect } from "react";
+// import { NavLink, useNavigate } from "react-router-dom";
+// import ButtonMain from "../components/UI/button/ButtonMain";
+// import MyInput from "../components/UI/input/MyInput";
 
-export const Registration = () => {
+// export const Registration = () => {
 
-  // управление значением, которое будет в инпуте
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  // // управление значением, которое будет в инпуте
+  // const [name, setName] = useState('')
+  // const [email, setEmail] = useState('')
+  // const [password, setPassword] = useState('')
 
-  //отражает состояние был ли курсор в инпуте
-  const [nameDirty, setNameDirty] = useState(false)
-  const [emailDirty, setEmailDirty] = useState(false)
-  const [passwordDirty, setPasswordDirty] = useState(false)
+  // //отражает состояние был ли курсор в инпуте
+  // const [nameDirty, setNameDirty] = useState(false)
+  // const [emailDirty, setEmailDirty] = useState(false)
+  // const [passwordDirty, setPasswordDirty] = useState(false)
 
-  // управление состояние при ошибке
-  const [nameError, setNameError] = useState('Поле имени не может быть пустым')
-  const [emailError, setEmailError] = useState('Поле еmail не может быть пустым')
-  const [passwordError, setPasswordError] = useState('Необходимо ввести пароль')
+  // // управление состояние при ошибке
+  // const [nameError, setNameError] = useState('Поле имени не может быть пустым')
+  // const [emailError, setEmailError] = useState('Поле еmail не может быть пустым')
+  // const [passwordError, setPasswordError] = useState('Необходимо ввести пароль')
 
-  // состояние валидна форма или нет
-  const [formValid, setFormValid] = useState(false)
+  // // состояние валидна форма или нет
+  // const [formValid, setFormValid] = useState(false)
 
 
-  useEffect(() => {
-    if (nameError || emailError || passwordError) {
-      setFormValid(false)
-    } else {
-      setFormValid(true)
-    }
-  }, [nameError, emailError, passwordError])
+  // useEffect(() => {
+  //   if (nameError || emailError || passwordError) {
+  //     setFormValid(false)
+  //   } else {
+  //     setFormValid(true)
+  //   }
+  // }, [nameError, emailError, passwordError])
 
-    // меняем дифолтное значение инпута, на то что вводит пользователь
-    const nameHandler = (e) => {
-      setName(e.target.value)
-      if (e.target.value.length < 1 || e.target.value.length > 25) {
-        setNameError('Имя должено состоять от 1 до 25 символов')
-        if (!e.target.value) {
-          setNameError('Необходимо ввести имя')
-        }
-      } else {
-        setNameError('')
-      }
-    }
+    // // меняем дифолтное значение инпута, на то что вводит пользователь
+    // const nameHandler = (e) => {
+    //   setName(e.target.value)
+    //   if (e.target.value.length < 1 || e.target.value.length > 25) {
+    //     setNameError('Имя должено состоять от 1 до 25 символов')
+    //     if (!e.target.value) {
+    //       setNameError('Необходимо ввести имя')
+    //     }
+    //   } else {
+    //     setNameError('')
+    //   }
+    // }
   // меняем дифолтное значение инпута, на то что вводит пользователь
-  const emailHandler = (e) => {
-    setEmail(e.target.value)
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    if (!re.test(String(e.target.value).toLowerCase())) {
-      setEmailError('Некорректный email')
-    } else {
-      setEmailError('')
-    }
-  }
-  // меняем дифолтное значение инпута, на то что вводит пользователь
-  const passwordlHandler = (e) => {
-    setPassword(e.target.value)
-    if (e.target.value.length < 3 || e.target.value.length > 15) {
-      setPasswordError('Пароль должен быть длиннее 3 и менее 15 символов')
-      if (!e.target.value) {
-        setPasswordError('Необходимо ввести пароль')
-      }
-    } else {
-      setPasswordError('')
-    }
-  }
+  // const emailHandler = (e) => {
+  //   setEmail(e.target.value)
+  //   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  //   if (!re.test(String(e.target.value).toLowerCase())) {
+  //     setEmailError('Некорректный email')
+  //   } else {
+  //     setEmailError('')
+  //   }
+  // }
+  // // меняем дифолтное значение инпута, на то что вводит пользователь
+  // const passwordlHandler = (e) => {
+  //   setPassword(e.target.value)
+  //   if (e.target.value.length < 3 || e.target.value.length > 15) {
+  //     setPasswordError('Пароль должен быть длиннее 3 и менее 15 символов')
+  //     if (!e.target.value) {
+  //       setPasswordError('Необходимо ввести пароль')
+  //     }
+  //   } else {
+  //     setPasswordError('')
+  //   }
+  // }
 
-  // момент, когда пользователь покинул поле ввода
-  const blurHandler = (e) => {
-    switch (e.target.name) {
-      case 'name' :
-        setNameDirty(true)
-        break
-      case 'email' :
-        setEmailDirty(true)
-        break
-      case 'password' : 
-        setPasswordDirty(true)
-        break
-    }
-  }
+  // // момент, когда пользователь покинул поле ввода
+  // const blurHandler = (e) => {
+  //   switch (e.target.name) {
+  //     case 'name' :
+  //       setNameDirty(true)
+  //       break
+  //     case 'email' :
+  //       setEmailDirty(true)
+  //       break
+  //     case 'password' : 
+  //       setPasswordDirty(true)
+  //       break
+  //   }
+  // }
   
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  const registrationUser = async() => {
+  // const registrationUser = async() => {
 
-    // fetch('http://127.0.0.1:8000/api/v1/user/register/', {
-    //   method: 'POST',
-    //   body: JSON.stringify({email,password}),
-    //   headers: {
-    //     'Content-Type': 'application/json'}
-    // })
+  //   // fetch('http://127.0.0.1:8000/api/v1/user/register/', {
+  //   //   method: 'POST',
+  //   //   body: JSON.stringify({email,password}),
+  //   //   headers: {
+  //   //     'Content-Type': 'application/json'}
+  //   // })
 
-    try {
-      const data = {first_name: name, email, password}
-      console.log(data);
-      const response = await axios.post('http://127.0.0.1:8000/api/v1/user/register/', data)
-      alert('Вы успешно зарегистрированы!')
-      if (response.data.succsses) {
-        localStorage.setItem('token', response.data.token)
-        navigate('/')
-        navigate(0)
-      }
-    } catch (error) {
-      alert(error.response.data);
-    }
-  }
+  //   try {
+  //     const data = {first_name: name, email, password}
+  //     console.log(data);
+  //     const response = await axios.post('http://127.0.0.1:8000/api/v1/user/register/', data)
+  //     alert('Вы успешно зарегистрированы!')
+  //     if (response.data.succsses) {
+  //       localStorage.setItem('token', response.data.token)
+  //       navigate('/')
+  //       navigate(0)
+  //     }
+  //   } catch (error) {
+  //     alert(error.response.data);
+  //   }
+  // }
 
-  return (
-    <>
-      <form 
-        style={{width: '300px'}}
-      >
-        <h1 style={{marginBottom: '25px'}}>Регистрация</h1>
+  // return (
+  //   <>
+  //     <form 
+  //       style={{width: '300px'}}
+  //     >
+  //       <h1 style={{marginBottom: '25px'}}>Регистрация</h1>
 
-        {(nameDirty && nameError) && <div style={{color: 'red'}}>{nameError}</div>}
-        <MyInput
-          value={name}
-          onChange={e => nameHandler(e)}
-          onBlur = {e => blurHandler(e)}
-          name="name"
-          type="text"
-          placeholder="Введите ваше имя"
-        />
+  //       {(nameDirty && nameError) && <div style={{color: 'red'}}>{nameError}</div>}
+  //       <MyInput
+  //         value={name}
+  //         onChange={e => nameHandler(e)}
+  //         onBlur = {e => blurHandler(e)}
+  //         name="name"
+  //         type="text"
+  //         placeholder="Введите ваше имя"
+  //       />
 
-        {(emailDirty && emailError) && <div style={{color: 'red'}}>{emailError}</div>}
-        <MyInput
-          value={email}
-          onChange={e => emailHandler(e)}
-          onBlur = {e => blurHandler(e)}
-          name="email"
-          type="text"
-          placeholder="Введите ваш email"
-        />
+  //       {(emailDirty && emailError) && <div style={{color: 'red'}}>{emailError}</div>}
+  //       <MyInput
+  //         value={email}
+  //         onChange={e => emailHandler(e)}
+  //         onBlur = {e => blurHandler(e)}
+  //         name="email"
+  //         type="text"
+  //         placeholder="Введите ваш email"
+  //       />
 
-        {(passwordDirty && passwordError) && <div style={{color: 'red'}}>{passwordError}</div>}
-        <MyInput 
-          value={password}
-          onChange={e => passwordlHandler(e)}
-          onBlur = {e => blurHandler(e)}
-          name="password" 
-          type="text" 
-          placeholder="Введите ваш пароль"
-        />
-        <ButtonMain 
-          onClick={() => registrationUser()}
-          className="btn btn-fiolet" 
-          disabled={!formValid} 
-          type='button'
-          >
-          зарегистрироваться
-        </ButtonMain>
-      </form>
+  //       {(passwordDirty && passwordError) && <div style={{color: 'red'}}>{passwordError}</div>}
+  //       <MyInput 
+  //         value={password}
+  //         onChange={e => passwordlHandler(e)}
+  //         onBlur = {e => blurHandler(e)}
+  //         name="password" 
+  //         type="text" 
+  //         placeholder="Введите ваш пароль"
+  //       />
+  //       <ButtonMain 
+  //         onClick={() => registrationUser()}
+  //         className="btn btn-fiolet" 
+  //         disabled={!formValid} 
+  //         type='button'
+  //         >
+  //         зарегистрироваться
+  //       </ButtonMain>
+  //     </form>
 
-      <div className="block-link">
-        <h3>Уже зарегистрированы?</h3>
-        <NavLink to="/sign-in">
-          перейти на страницу для входа
-        </NavLink>
-      </div>
-    </>
-  )}
+  //     <div className="block-link">
+  //       <h3>Уже зарегистрированы?</h3>
+  //       <NavLink to="/sign-in">
+  //         перейти на страницу для входа
+  //       </NavLink>
+  //     </div>
+  //   </>
+  // )}
 
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -6,35 +6,38 @@ import MyInput from "../components/UI/input/MyInput";
 
 export const SignIn = () => {
 
-// управление значением, которое будет в инпуте
-const [email, setEmail] = useState('')
-const [password, setPassword] = useState('')
+const navigate = useNavigate();
+
+const [dataUser, setDataUser] = useState({email: '', password: '', name: ''})
 
 //отражает состояние был ли курсор в инпуте
-const [emailDirty, setEmailDirty] = useState(false)
-const [passwordDirty, setPasswordDirty] = useState(false)
+const [inputVisit, setInputVisit] = useState({
+  inputEmail: false, 
+  inputPassword: false, 
+  inputName: false
+})
 
 // управление состояние при ошибке
 const [emailError, setEmailError] = useState('Email не может быть пустым')
 const [passwordError, setPasswordError] = useState('Необходимо ввести пароль')
+const [nameError, setNameError] = useState('Поле имени не может быть пустым')
 
 // состояние валидна форма или нет
 const [formValid, setFormValid] = useState(false)
 
 const [errorResponse, setErrorResponse] = useState('')
 
-
 useEffect(() => {
-  if (emailError || passwordError) {
+  if (emailError || passwordError || nameError) {
     setFormValid(false)
   } else {
     setFormValid(true)
   }
-}, [emailError, passwordError])
+}, [emailError, passwordError, nameError])
 
 // меняем дифолтное значение инпута, на то что вводит пользователь
 const emailHandler = (e) => {
-  setEmail(e.target.value)
+  setDataUser({...dataUser, email: e.target.value})
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   if (!re.test(String(e.target.value).toLowerCase())) {
     setEmailError('Некорректный email')
@@ -45,7 +48,7 @@ const emailHandler = (e) => {
 
 // меняем дифолтное значение инпута, на то что вводит пользователь
 const passwordlHandler = (e) => {
-  setPassword(e.target.value)
+  setDataUser({...dataUser, password: e.target.value})
   if (e.target.value.length < 3 || e.target.value.length > 15) {
     setPasswordError('Пароль должен быть длиннее 3 и менее 15 символов')
     if (!e.target.value) {
@@ -56,23 +59,35 @@ const passwordlHandler = (e) => {
   }
 }
 
+const nameHandler = (e) => {
+  setDataUser({...dataUser, name: e.target.value})
+  if (e.target.value.length < 1 || e.target.value.length > 25) {
+    setNameError('Имя должено содержать от 1 до 25 символов')
+    if (!e.target.value) {
+      setNameError('Необходимо ввести имя')
+    }
+  } else {
+    setNameError('')
+  }
+}
+
 // момент, когда пользователь покинул поле ввода
 const blurHandler = (e) => {
   switch (e.target.name) {
     case 'email' :
-      setEmailDirty(true)
+      setInputVisit({...inputVisit, inputEmail: true})
       break
     case 'password' : 
-      setPasswordDirty(true)
+    setInputVisit({...inputVisit, inputPassword: true})
       break
+    case 'name' :
+      setInputVisit({...inputVisit, inputName: true})
   }
 }
 
-const navigate = useNavigate();
-
 const signInUser = async() => {
   try {
-    const data = {email, password}
+    const data = {email: dataUser.email, password: dataUser.password}
     const response = await axios.post('http://127.0.0.1:8000/api/v1/user/login/', data)
     //alert('пользователь найден')
     if (response.data.succsses) {
@@ -85,46 +100,129 @@ const signInUser = async() => {
   }
 }
 
+const registrationUser = async() => {
+  try {
+    const data = {first_name: dataUser.name, email: dataUser.email, password: dataUser.password}
+    //console.log(data);
+    const response = await axios.post('http://127.0.0.1:8000/api/v1/user/register/', data)
+    alert('Вы успешно зарегистрированы!')
+    if (response.data.succsses) {
+      localStorage.setItem('token', response.data.token)
+      navigate('/')
+      navigate(0)
+    }
+  } catch (error) {
+    setErrorResponse(error?.response.data)
+  }
+}
+
+const [openBlockRegistration, setOpenBlockRegistration] = useState(true);
+const onClickOpenBlockRegistration = () => {
+  setOpenBlockRegistration(!openBlockRegistration)
+};
+
   return (
     <>
-      <form style={{width: '300px'}}>
-        <h1 style={{marginBottom: '25px'}}>Вход</h1>
 
-        { (!!errorResponse) && <div style={{color: 'red'}}>{errorResponse}</div>}
+      { !openBlockRegistration ?
 
-        {(emailDirty && emailError) && <div style={{color: 'red'}}>{emailError}</div>}
-        <MyInput
-          value={email}
-          onChange={e => emailHandler(e)}
-          onBlur = {e => blurHandler(e)}
-          name="email"
-          type="text"
-          placeholder="Введите ваш email"
-        />
-        {(passwordDirty && passwordError) && <div style={{color: 'red'}}>{passwordError}</div>}
-        <MyInput 
-          value={password}
-          onChange={e => passwordlHandler(e)}
-          onBlur = {e => blurHandler(e)}
-          name="password" 
-          type="text" 
-          placeholder="Введите ваш пароль"
-        />
-        <ButtonMain 
-          onClick={() => signInUser()}
-          className="btn btn-fiolet" 
-          disabled={!formValid} 
-          type='button'
+        <div>
+          <form style={{width: '300px'}}>
+            <h1 style={{marginBottom: '25px'}}>Вход</h1>
+            { (!!errorResponse) && <div style={{color: 'red'}}>{errorResponse}</div>}
+
+            {(inputVisit.inputEmail && emailError) && <div style={{color: 'red'}}>{emailError}</div>}
+            <MyInput
+              value={dataUser.email}
+              onChange={e => emailHandler(e)}
+              onBlur = {e => blurHandler(e)}
+              name="email"
+              type="text"
+              placeholder="Введите ваш email"
+            />
+            {(inputVisit.inputPassword && passwordError) && <div style={{color: 'red'}}>{passwordError}</div>}
+            <MyInput 
+              value={dataUser.password}
+              onChange={e => passwordlHandler(e)}
+              onBlur = {e => blurHandler(e)}
+              name="password" 
+              type="text" 
+              placeholder="Введите ваш пароль"
+            />
+            <ButtonMain 
+              onClick={() => signInUser()}
+              className="btn btn-fiolet" 
+              disabled={!formValid} 
+              type='button'
+              >
+              войти
+            </ButtonMain>
+          </form>
+          <div className="block-link">
+            <h3>Еще нет учетной записи?</h3>
+            <NavLink onClick={onClickOpenBlockRegistration}>
+              перейти для регистрации
+            </NavLink>
+          </div>
+        </div>
+
+        :
+
+        <div>
+          <form 
+            style={{width: '300px'}}
           >
-          войти
-        </ButtonMain>
-      </form>
+            <h1 style={{marginBottom: '25px'}}>Регистрация</h1>
+            { (!!errorResponse) && <div style={{color: 'red'}}>{errorResponse}</div>}
 
-      <div className="block-link">
-        <h3>Еще нет учетной записи?</h3>
-        <NavLink to="/registration">
-          перейти для регистрации
-        </NavLink>
-      </div>
+            {(inputVisit.inputName && nameError) && <div style={{color: 'red'}}>{nameError}</div>}
+            <MyInput
+              value={dataUser.name}
+              onChange={e => nameHandler(e)}
+              onBlur = {e => blurHandler(e)}
+              name="name"
+              type="text"
+              placeholder="Введите ваше имя"
+            />
+
+            {(inputVisit.inputEmail && emailError) && <div style={{color: 'red'}}>{emailError}</div>}
+            <MyInput
+              value={dataUser.email}
+              onChange={e => emailHandler(e)}
+              onBlur = {e => blurHandler(e)}
+              name="email"
+              type="text"
+              placeholder="Введите ваш email"
+            />
+
+            {(inputVisit.inputPassword && passwordError) && <div style={{color: 'red'}}>{passwordError}</div>}
+            <MyInput 
+              value={dataUser.password}
+              onChange={e => passwordlHandler(e)}
+              onBlur = {e => blurHandler(e)}
+              name="password" 
+              type="text" 
+              placeholder="Введите ваш пароль"
+            />
+            <ButtonMain 
+              onClick={() => registrationUser()}
+              className="btn btn-fiolet" 
+              disabled={!formValid} 
+              type='button'
+              >
+              зарегистрироваться
+            </ButtonMain>
+          </form>
+
+          <div className="block-link">
+            <h3>Уже зарегистрированы?</h3>
+            <NavLink onClick={onClickOpenBlockRegistration}>
+              перейти на страницу для входа
+            </NavLink>
+          </div>
+        </div>
+
+      }
+      
     </>
   )}
